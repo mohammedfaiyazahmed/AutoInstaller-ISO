d9a4ae8f7456c0ee807cf1f94196f72c0678a43f
mohammedfaiyazahmed <mohammed.faiyaz.ahmed@intel.com>
2024-05-23 10:41:53 +0530

    commit 1

--- Hyper V/Windows/make_iso6.py
+++ Hyper V/Windows/make_iso6.py
@@ -5,7 +5,7 @@ import os
 autoattend_url = "https://wiki.ith.intel.com/download/attachments/2518244265/autounattend.xml?version=1&modificationDate=1659385714323&api=v2"
 windows_iso_url = "https://ubit-artifactory-ba.intel.com/artifactory/dcg-dea-srvplat-local/Kits/BHS-GNR-SP-WIN2022/BHS-GNR-SP-WIN2022-24.21.2.4/Images/20348.2322_13feb2024_server_eval_x64fre_en-us_gnr_bkc_FRE_IA-64_BHS-GNR-SP-WIN2022-24.21.2.4.iso"
 
-download_directory = os.path.join(os.getcwd(), "downloads")
+download_directory = os.path.join(os.getcwd(), "../../downloads")
 if not os.path.exists(download_directory):
     os.makedirs(download_directory)
 
