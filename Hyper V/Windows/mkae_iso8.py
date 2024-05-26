04308afa906df1503300251fa31b00945d155f45
mohammedfaiyazahmed <mohammed.faiyaz.ahmed@intel.com>
2024-05-27 00:50:04 +0530

    commit 1

--- Hyper V/Windows/mkae_iso8.py
+++ Hyper V/Windows/mkae_iso8.py
@@ -1,12 +1,7 @@
 import os
 import requests
-import subprocess
-from pycdlib import PyCdlib
-import requests as q
 from requests.auth import HTTPBasicAuth
-from tqdm import tqdm
-
-print("*******", q.certs.where())
+from tqdm import tqdm  # Import tqdm
 
 download_directory = os.path.join(os.getcwd(), "downloads")
 if not os.path.exists(download_directory):
@@ -17,7 +12,6 @@ windows_iso_url = "https://ubit-artifactory-ba.intel.com/artifactory/dcg-dea-srv
 
 autoattend_file = os.path.join(download_directory, "autounattend.xml")
 windows_iso_file = os.path.join(download_directory, "windows.iso")
-modified_iso_file = os.path.join(download_directory, "modified_windows.iso")
 
 def download_file(url, filename):
     proxies = {
@@ -25,10 +19,10 @@ def download_file(url, filename):
         "https": "",
     }
     print("current file path:", filename)
-    with requests.get(url, stream=True, proxies=proxies, verify=False, auth = HTTPBasicAuth('sys_degsi1', 'zaq1@wsxcde34rfvbgt5')) as r:
+    with requests.get(url, stream=True, proxies=proxies, verify=False, auth=HTTPBasicAuth('sys_degsi1', 'zaq1@wsxcde34rfvbgt5')) as r:
         r.raise_for_status()
         total_size_in_bytes = int(r.headers.get('content-length', 0))
-        block_size = 8192 
+        block_size = 8192  # 1 Kibibyte
         progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
         with open(filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=block_size):
