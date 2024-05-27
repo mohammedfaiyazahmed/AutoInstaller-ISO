67650d0e85f5471dcfec5a0cd2ddb8b50450d65b
mohammedfaiyazahmed <mohammed.faiyaz.ahmed@intel.com>
2024-05-27 18:12:33 +0530

    commit 1

--- Hyper V/Windows/make_iso21.py
+++ Hyper V/Windows/make_iso21.py
@@ -2,10 +2,11 @@ import os
 import shutil
 import subprocess
 import requests
+import urllib.parse
 
 downloads_folder = os.path.expanduser('C:\\Users\\General\\PycharmProjects\\AutoInstaller-ISO\\downloads')
 iso_path = os.path.join(downloads_folder, 'windows.iso')
-autoattend_path = os.path.join(downloads_folder, 'autoattend.xml')
+autoattend_path = os.path.join(downloads_folder, 'autounattend.xml')
 extracted_iso_folder = os.path.join(downloads_folder, 'extracted_iso')
 new_iso_path = os.path.join(downloads_folder, 'new_windows_vm.iso')
 imgburn_installer_path = os.path.join(downloads_folder, 'SetupImgBurn.exe')
@@ -14,7 +15,12 @@ seven_zip_installer_path = os.path.join(downloads_folder, '7z1900-x64.exe')
 seven_zip_executable = 'C:\\Program Files\\7-Zip\\7z.exe'
 
 url_7zip = 'https://www.7-zip.org/a/7z1900-x64.exe'
-url_imgburn = 'http://download.imgburn.com/SetupImgBurn_2.5.8.0.exe'
+url_imgburn = 'https://ubit-artifactory-ba.intel.com/artifactory/dcg-dea-srvplat-local/Automation_Tools/PAIV_DevOps/Faiyaz/SetupImgBurn_2.5.8.0.exe'
+
+proxies = {
+        "http": "",
+        "https": "",
+    }
 
 def download_and_install_7zip():
     response = requests.get(url_7zip)
@@ -26,11 +32,11 @@ def extract_iso(iso_path, extract_to):
     subprocess.run([seven_zip_executable, 'x', iso_path, f'-o{extract_to}'], check=True)
 
 def add_autoattend(extracted_folder, autoattend_file):
-    destination = os.path.join(extracted_folder, 'autoattend.xml')
+    destination = os.path.join(extracted_folder, 'autounattend.xml')
     shutil.copyfile(autoattend_file, destination)
 
 def download_and_install_imgburn():
-    response = requests.get(url_imgburn)
+    response = requests.get(url_imgburn, proxies = proxies)
     with open(imgburn_installer_path, 'wb') as file:
         file.write(response.content)
     subprocess.run([imgburn_installer_path, '/S'], check=True)
